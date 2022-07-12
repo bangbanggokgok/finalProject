2f636b673aa736c2838b40797905820ce92d6f9d
mothership <mothership2002@naver.com>
2022-07-12 13:09:22 +0900

    로그인, 회원가입 완료

--- src/main/java/edu/kh/bangbanggokgok/controller/user/LoginController.java
+++ src/main/java/edu/kh/bangbanggokgok/controller/user/LoginController.java
@@ -2,17 +2,21 @@ package edu.kh.bangbanggokgok.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.bangbanggokgok.service.user.UserService;
 import edu.kh.bangbanggokgok.service.user.UserServiceImpl;
+import edu.kh.bangbanggokgok.vo.user.User;
 
 @Controller
 @RequestMapping("user/*")
+@SessionAttributes({ "loginUser" })
 public class LoginController {
 
 	@Autowired
@@ -26,17 +30,19 @@ public class LoginController {
 	@PostMapping("login")
 	public String login(@RequestParam(value = "email") String email,
 			@RequestParam(value = "password") String password,
-			RedirectAttributes ra) {
+			RedirectAttributes ra, Model model) {
 		
-		int result = service.login(email,password);
-		
-		if(result>0) {
+		User result = service.login(email,password);
+		String path = "";
+		if(result!=null) {
+			model.addAttribute("loginUser",result);
 			ra.addFlashAttribute("message","로그인 성공");
+			path = "redirect:/";
 		} else {
 			ra.addFlashAttribute("message","실패");
+			path = "redirect:/user/login";
 		}
-		
-		return "redirect:/";
+		return path;
 	}
 
 	@PostMapping("find-account")
