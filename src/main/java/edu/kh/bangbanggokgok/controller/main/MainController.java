be2cad40d6e1a0482f9b8e67c55cd87361b31aba
mothership <mothership2002@naver.com>
2022-07-14 09:32:57 +0900

    l

--- src/main/java/edu/kh/bangbanggokgok/controller/main/MainController.java
+++ src/main/java/edu/kh/bangbanggokgok/controller/main/MainController.java
@@ -2,6 +2,7 @@ package edu.kh.bangbanggokgok.controller.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Controller
 public class MainController {
@@ -10,4 +11,8 @@ public class MainController {
 	public String main() {
 		return "main";
 	}
+	
+//	메인에 로케이션 긁어와야할듯
+	
+	
 }
---
+++ src/main/java/edu/kh/bangbanggokgok/vo/board/Location.java
@@ -0,0 +1,16 @@
+package edu.kh.bangbanggokgok.vo.board;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Location {
+	
+	private int locationNum;
+	private String locationName;
+}
