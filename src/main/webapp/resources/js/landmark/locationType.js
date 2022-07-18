4efc1de3e8874974be72aaad2c0ebff357206049
mothership <mothership2002@naver.com>
2022-07-18 14:25:18 +0900

    랜드마크 처리 (중위)

--- src/main/webapp/resources/js/landmark/locationType.js
+++ src/main/webapp/resources/js/landmark/locationType.js
@@ -3,32 +3,28 @@ function searchingLocation(locationNum){
     $.ajax({
         url: "list/"+locationNum,
         type: "GET",
-        data: { "locationNum": locationNum
-                 },
+        data: { "locationNum": locationNum },
         dataType: "JSON",
-        success:function(landList){
+        success:function(result){
+            
+            const landMarkList = document.getElementById("landMarkList");
 
-            const landMakrList = doument.getElementById(landMakrList);
-
-            for(let landMakrList of landList){
+            for(let landMark of result.landMarkList){
                 
-                const landRow = document.createElement("li");
-                landRow.classList.add("land-row");
+                // const landRow = document.createElement("li");
+                // landRow.classList.add("land-row");
 
 
-                const landMarkImage =document.createElement("img");
-                 landMarkImage.setAttribute("src", contextPath + landMark.landMarkImage);
+                // const landMarkImage = document.createElement("img");
+                // landMarkImage.setAttribute("src", contextPath + landMark.landMarkImage);
 
-                const landMarkContent = document.createElement("figcaption");
-                landMarkContent.classList.add("land-Title");
-                landMarkContent.classList.add("land-content");
+                // const landMarkContent = document.createElement("figcaption");
+                // landMarkContent.classList.add("land-Title");
+                // landMarkContent.classList.add("land-content");
                 
-
-
+                // landMarkList.append(landRow);
+                // 요소 생성은 여기서 차근차근 해보십시오
             }
-
-            landMakrList.append(landRow);
-
         }
     })
 }
