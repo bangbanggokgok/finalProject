782f4ffcc67302e083570ace3b88fedc3b63fbda
mothership <mothership2002@naver.com>
2022-07-21 20:50:27 +0900

    관리자 공지타입 밸류 갱신

--- src/main/webapp/resources/js/admin_/notice-write.js
+++ src/main/webapp/resources/js/admin_/notice-write.js
@@ -1,15 +1,28 @@
+//input 히든태그에 밸류 넣기
+// function noticeType(num){
+//     const a = document.getElementsByName("noticeType")[0];
+//     if(num>0){
+//         a.value = "notice";
+//     };
+//     if(num=0){
+//         a.value = "event";
+//     };
+//     console.log(a.value);
+// };
+
+
 function drop_choiceFields(){
 	let click = document.getElementById("choiceFieldsContainer");
 	let icon = document.getElementById("inputField");
 	if(click.style.display == "none"){
 		click.style.display = "block";
 		
-		icon.innerHTML = "<i class='fal fa-angle-up'></i>";
+		// icon.innerHTML = "<i class='fal fa-angle-up'></i>";
 	}else{
 		click.style.display = "none";
-		icon.innerHTML = "<i class='fal fa-angle-down'></i>";
+		// icon.innerHTML = "<i class='fal fa-angle-down'></i>";
 	}
-}
+};
 
 
 (function(){
@@ -17,22 +30,23 @@ function drop_choiceFields(){
     const notice = document.getElementById("notice"); 
     const event = document.getElementById("event"); 
     const title = document.getElementById("title");
+    const a = document.getElementsByName("noticeType")[0];
 	notice.addEventListener("click", function(){
 		
 		title.innerText = "공지";
 		click.style.display = "none";
-		title.value = "notice";
+		a.value = "notice";
 
-		console.log(title.value);
+		console.log(a.value);
 	});
 
 	event.addEventListener("click", function(){
 		
 		title.innerText = "이벤트";
 		click.style.display = "none";
-		event.value = "event";
+		a.value = "event";
 
-		console.log(event.value);
+		console.log(a.value);
 	});
     
 })();
@@ -49,54 +63,28 @@ const deleteList = document.getElementById("deleteList");
 // 게시글 수정 시 삭제된 이미지의 레벨(위치)를 기록해둘 Set 생성
 const deleteSet = new Set(); // 순서 X, 중복 허용 X (여러번 클릭 시 중복 값 저장 방지)
 
-
-
 for(let i=0 ; i<inputImage.length ; i++){
-
-    // 파일이 선택 되었을 때의 동작
     inputImage[i].addEventListener("change", function(){
-
-        if(this.files[0] != undefined){ // 파일이 선택 되었을 때
-            const reader = new FileReader(); // 선택된 파일을 읽을 객체 생성
+        if(this.files[0] != undefined){
+            const reader = new FileReader();
             reader.readAsDataURL(this.files[0]);
-            // 지정된 파일을 읽음 -> result에 저장(URL 포함) -> URL을 이용해서 이미지 볼 수 있음
-
-            reader.onload = function(e){ // reader가 파일을 다 읽어온 경우
-                // e.tartget == redaer
-                // e.target.result == 읽어들인 이미지의 URL
-                // preview[i] == 파일이 선택된 input태그와 인접한 preview 이미지 태그
+            reader.onload = function(e){
                 preview[i].setAttribute("src", e.target.result);
-
-                // 이미지가 성공적으로 불러와졌을 때
-                // deleteSet에서 해당 레벨을 제거(삭제 목록에서 제외)
                 deleteSet.delete(i);
-
-				icon[i].remove();
+				// icon[i].remove();
             }
       
-        } else { // 파일이 선택되지 않았을 때 (취소)
-            preview[i].removeAttribute("src"); // src 속성 제거
+        } else { 
+            preview[i].removeAttribute("src");
         }
     });
 
-
-
-    // 미리보기 삭제 버튼(x)이 클릭 되었을 때의 동작
     deleteImage[i].addEventListener("click", function(){
 
-        // 미리보기가 존재하는 경우에만 (이전에 추가된 이미지가 있을 때만 X버튼 동작)
         if( preview[i].getAttribute("src")  !=  "" ){
-
-            // 미리보기 삭제
             preview[i].removeAttribute("src");
-
-            // input의 값을 "" 만들기
             inputImage[i].value = "";
-
-            // deleteSet에 삭제된 이미지 레벨(i) 추가
             deleteSet.add(i);
         }
-
     });
-
-}
+};
