2913b74822af8937acfff8c97924ca6ccae0b33a
park-sm <nambawon1@naver.com>
2022-07-24 14:18:25 +0900

    별점

--- src/main/webapp/resources/js/landmark/script.js
+++ src/main/webapp/resources/js/landmark/script.js
@@ -90,3 +90,7 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+const drawStar = (target) => {
+  document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
+};
