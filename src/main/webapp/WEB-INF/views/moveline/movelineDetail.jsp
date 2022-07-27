71a4c04c5b04753680eeb2370d6753946cd2ad26
park-sm <nambawon1@naver.com>
2022-07-27 17:21:42 +0900

    moveline 즐겨찾기 버튼 수정

--- src/main/webapp/WEB-INF/views/moveline/movelineDetail.jsp
+++ src/main/webapp/WEB-INF/views/moveline/movelineDetail.jsp
@@ -69,7 +69,11 @@
                     <div id="top-bottom-area">
                         <div>
                             <button type="button" class="btn-bookmark" onclick="setBookmark(${movelineNo});">
-                                <span class="icon-bookmark">즐겨찾기</span>
+                                <span class="icon-bookmark">
+                                    <img class='bookmarkOff' src="${contextPath}/resources/images/movelineDetail/즐겨찾기off.png/" alt="즐겨찾기off">
+                                    <img class='bookmarkOn' src="${contextPath}/resources/images/movelineDetail/즐겨찾기on.png/" alt="즐겨찾기ofn">
+                                </span>
+                             
                             </button>
                         </div>
                         <div id="top-bottom">
--- src/main/webapp/resources/js/moveline/movelineDetail.js
+++ src/main/webapp/resources/js/moveline/movelineDetail.js
@@ -2,216 +2,192 @@ const landmark = document.getElementsByClassName("landmark");
 const selected = document.getElementsByClassName("selected-landmark");
 const showContent = document.querySelector(".showContent");
 const container2 = document.querySelector(".container2");
+const bookmarkBtn = document.querySelector(".btn-bookmark");
+const bookmarkOn = document.querySelector(".bookmarkOn");
+const bookmarkOff = document.querySelector(".bookmarkOff");
+
+bookmarkBtn.addEventListener("click", () => {
+  bookmarkOn.classList.toggle("active");
+  bookmarkOff.classList.toggle("d-none");
+});
 
 let landmarkNo;
 
 // 특정 랜드마크 이미지 세팅
 for (let i = 0; i < landmark.length; i++) {
-    landmark[i].addEventListener("click", function () {
+  landmark[i].addEventListener("click", function () {
+    landmarkNo = this.getAttribute("value");
 
-        landmarkNo = this.getAttribute("value");
+    // var click_val = $("#id").val();
+    // alert(click_val);
 
-        // var click_val = $("#id").val();
-        // alert(click_val);
+    // alert(this.getAttribute("id"));
+    // alert(this.getAttribute("value"));
 
-        // alert(this.getAttribute("id")); 
-        // alert(this.getAttribute("value")); 
+    $.ajax({
+      url: contextPath + "/moveline-main/detail/setLandmarkImages",
+      data: { landmarkNo: this.getAttribute("value") },
+      type: "get",
+      dataType: "JSON",
+      success: function (landmarkImageList) {
+        alert(landmarkImageList);
+        // alert(landmarkContentList);
 
-        $.ajax({
-            url: contextPath + "/moveline-main/detail/setLandmarkImages",
-            data: { "landmarkNo": this.getAttribute("value") },
-            type: "get",
-            dataType: "JSON",
-            success: function (landmarkImageList) {
+        console.log("size : " + landmarkImageList[0].landMarkReName);
+        const entire = document.getElementById("entire"); // 사진 아래 제거
+        // const test2 = document.getElementById("landmark-explain"); // 상세설명 내용 제거
+        entire.innerHTML = "";
+        // test2.innerHTML = "";
 
-                alert(landmarkImageList);
-                // alert(landmarkContentList);
+        // test1.style.left = "0%";
+        const test1 = document.createElement("ul");
+        test1.classList.add("slider-container2", "simple-list", "slider2");
+        test1.setAttribute("id", "test1");
 
-                console.log("size : " + landmarkImageList[0].landMarkReName);
-                const entire = document.getElementById("entire"); // 사진 아래 제거
-                // const test2 = document.getElementById("landmark-explain"); // 상세설명 내용 제거
-                entire.innerHTML = "";
-                // test2.innerHTML = "";
+        entire.append(test1);
 
-                // test1.style.left = "0%";
-                const test1 = document.createElement("ul");
-                test1.classList.add("slider-container2", "simple-list", "slider2");
-                test1.setAttribute("id", "test1");
+        let leftSize = 0;
 
-                entire.append(test1);
+        // 행
+        for (let simpleList of landmarkImageList) {
+          console.log("reName Log : " + simpleList.landMarkReName);
+          const slide2 = document.createElement("li");
+          slide2.setAttribute("id", "landmark-images");
+          slide2.classList.add("slide2");
 
-                let leftSize = 0;
+          slide2.style.left = leftSize + "%";
+          leftSize += 100;
 
-                // 행
-                for (let simpleList of landmarkImageList) {
-                    console.log("reName Log : " + simpleList.landMarkReName);
-                    const slide2 = document.createElement("li");
-                    slide2.setAttribute("id", "landmark-images");
-                    slide2.classList.add("slide2");
+          const slideImg2 = document.createElement("img");
+          slideImg2.classList.add("slide-img2");
+          slideImg2.setAttribute(
+            "src",
+            contextPath + simpleList.landMarkReName
+          );
 
-                    slide2.style.left = (leftSize)  + "%";
-                    leftSize += 100;
+          test1.append(slide2);
+          slide2.append(slideImg2);
+        }
 
-                    const slideImg2 = document.createElement("img");
-                    slideImg2.classList.add("slide-img2");
-                    slideImg2.setAttribute("src", contextPath + simpleList.landMarkReName);
+        let pager = document.createElement("p");
+        pager.classList.add("pager2");
 
-                    test1.append(slide2);
-                    slide2.append(slideImg2);
-                }
+        entire.append(pager);
 
+        for (let i = 0; i < landmarkImageList.length; i++) {
+          const span = document.createElement("span");
+          span.setAttribute("data-index", i);
+          span.innerText = i + 1;
 
-                let pager = document.createElement("p");
-                pager.classList.add("pager2");
+          if (i == 0) {
+            span.classList.add("active");
+          }
+          pager.append(span);
 
-                entire.append(pager);
+          span.addEventListener("click", function (event) {
+            var pagerNum2 = event.target.innerText - 1;
+            goToSlide2(pagerNum2);
+          });
+        }
 
+        const prev2 = document.createElement("a");
+        prev2.classList.add("prev2");
 
+        const left = document.createElement("li");
+        left.classList.add("fa-solid", "fa-angle-left");
 
-                for (let i= 0 ; i<landmarkImageList.length ; i++) {
-                    const span = document.createElement("span");
-                    span.setAttribute("data-index", i);
-                    span.innerText = i+1;
+        prev2.append(left);
 
-                    if(i == 0){
-                        span.classList.add("active");
-                    }
-                    pager.append(span);
+        const next2 = document.createElement("a");
+        next2.classList.add("next2");
 
-                    span.addEventListener('click',function(event){ 
-                        var pagerNum2 = event.target.innerText - 1;
-                        goToSlide2(pagerNum2);
-                    });
-                }
+        const right = document.createElement("li");
+        right.classList.add("fa-solid", "fa-angle-right");
 
-                const prev2 = document.createElement("a");
-                prev2.classList.add("prev2");
+        next2.append(right);
 
-                const left= document.createElement("li");
-                left.classList.add("fa-solid", "fa-angle-left");
+        entire.append(prev2, next2);
 
-                prev2.append(left);
+        $pager2 = document.querySelector(".pager2");
+        $slide2 = document.querySelectorAll(".slide2");
+        $currentIndex2 = 0;
+        $slideCount2 = $slide2.length;
+        $pagerBtn2 = document.querySelectorAll(".pager2 span");
 
-                const next2 = document.createElement("a");
-                next2.classList.add("next2");
+        $slideContainer2 = document.querySelector(".slider-container2");
 
-                const right = document.createElement("li");
-                right.classList.add("fa-solid", "fa-angle-right");
+        // for(let content of landMarkContent){
+        //     console.log("content log : " + content.landMarkContent)
 
-                next2.append(right);
+        // }
 
-                entire.append(prev2, next2);
+        prev2.addEventListener("click", function () {
+          //goToSlide($currentIndex - 1);
 
-                
-                
-
-
-
-                $pager2 = document.querySelector('.pager2');
-                $slide2 = document.querySelectorAll('.slide2');
-                $currentIndex2 = 0;
-                $slideCount2 = $slide2.length;
-                $pagerBtn2 = document.querySelectorAll('.pager2 span');
-
-                $slideContainer2 = document.querySelector('.slider-container2');
-
-
-
-
-
-                // for(let content of landMarkContent){
-                //     console.log("content log : " + content.landMarkContent)
-
-                // }
-
-                prev2.addEventListener('click',function(){            
-                    //goToSlide($currentIndex - 1);
-        
-                    if($currentIndex2 == 0){
-                        //$navPrev.classList.add('disabled');
-                        goToSlide2($slideCount2 - 1);
-                    }else{
-                        //$navPrev.classList.remove('disabled');
-                        goToSlide2($currentIndex2 - 1);
-                    } 
-                });
-        
-                next2.addEventListener('click',function(){
-                    //goToSlide($currentIndex + 1);
-        
-                   if($currentIndex2 == $slideCount2 - 1){
-                       // $navNext.classList.add('disabled');
-                       goToSlide2(0);
-                   }else{
-                       // $navNext.classList.remove('disabled');
-                       goToSlide2($currentIndex2 + 1);
-                   }
-                });
-
-
-
-            },
-
-            error: function (req, status, error) {
-                console.log("실패")
-                console.log(req.responseText);
-            }
+          if ($currentIndex2 == 0) {
+            //$navPrev.classList.add('disabled');
+            goToSlide2($slideCount2 - 1);
+          } else {
+            //$navPrev.classList.remove('disabled');
+            goToSlide2($currentIndex2 - 1);
+          }
         });
+
+        next2.addEventListener("click", function () {
+          //goToSlide($currentIndex + 1);
+
+          if ($currentIndex2 == $slideCount2 - 1) {
+            // $navNext.classList.add('disabled');
+            goToSlide2(0);
+          } else {
+            // $navNext.classList.remove('disabled');
+            goToSlide2($currentIndex2 + 1);
+          }
+        });
+      },
+
+      error: function (req, status, error) {
+        console.log("실패");
+        console.log(req.responseText);
+      },
     });
+  });
 }
-
-
-    
-
-
-
-
-
-
-
-
-
 
 // 상세설명 비동기
 showContent.addEventListener("click", function () {
+  alert(landmarkNo);
 
-    alert(landmarkNo)
-    
-    $.ajax({
-        url: contextPath + "/moveline-main/detail/setLandmarkContent",
-        data: { "landmarkNo": landmarkNo },
-        type: "get",
-        dataType: "JSON",
-        success: function (landmarkContent) {
-            
-            const entire = document.getElementById("entire"); // 상세설명 내용 제거
-            entire.innerHTML = "";
+  $.ajax({
+    url: contextPath + "/moveline-main/detail/setLandmarkContent",
+    data: { landmarkNo: landmarkNo },
+    type: "get",
+    dataType: "JSON",
+    success: function (landmarkContent) {
+      const entire = document.getElementById("entire"); // 상세설명 내용 제거
+      entire.innerHTML = "";
 
-            console.log(landmarkContent)
+      console.log(landmarkContent);
 
-            const test1 = document.createElement("ul");
-            test1.setAttribute("id", "test1")
-            test1.classList.add(".slider-container2",".simple-list",".slider2")
+      const test1 = document.createElement("ul");
+      test1.setAttribute("id", "test1");
+      test1.classList.add(".slider-container2", ".simple-list", ".slider2");
 
+      const contentArea = document.createElement("div");
+      contentArea.setAttribute("id", "landmark-explain");
 
-            const contentArea = document.createElement("div");
-            contentArea.setAttribute("id", "landmark-explain");
+      const inputContent = document.createElement("li");
+      inputContent.classList.add(".landmark");
+      inputContent.innerHTML = landmarkContent;
 
-            const inputContent = document.createElement("li");
-            inputContent.classList.add(".landmark");
-            inputContent.innerHTML = landmarkContent;
+      console.log(landmarkContent);
 
-            console.log(landmarkContent);
-
-            entire.append(test1);
-            test1.append(contentArea);
-            contentArea.append(inputContent);
-
-        }
-    })
+      entire.append(test1);
+      test1.append(contentArea);
+      contentArea.append(inputContent);
+    },
+  });
 });
-
-
 
 // showImage.addEventListener("click", function () {
 //     alert(landmarkNo)
@@ -221,8 +197,8 @@ showContent.addEventListener("click", function () {
 //     // var click_val = $("#id").val();
 //     // alert(click_val);
 
-//     // alert(this.getAttribute("id")); 
-//     // alert(this.getAttribute("value")); 
+//     // alert(this.getAttribute("id"));
+//     // alert(this.getAttribute("value"));
 
 //     $.ajax({
 //         url: contextPath + "/moveline-main/detail/setLandmarkImages",
@@ -234,10 +210,9 @@ showContent.addEventListener("click", function () {
 //             const entire = document.getElementById("entire"); // 상세설명 내용 제거
 //             entire.innerHTML = "";
 
-
 //         }
 //     });
-// });    
+// });
 
 //     showImage.addEventListener("click", function () {
 //     alert(landmarkNo)
@@ -247,8 +222,8 @@ showContent.addEventListener("click", function () {
 //     // var click_val = $("#id").val();
 //     // alert(click_val);
 
-//     // alert(this.getAttribute("id")); 
-//     // alert(this.getAttribute("value")); 
+//     // alert(this.getAttribute("id"));
+//     // alert(this.getAttribute("value"));
 
 //     $.ajax({
 //         url: contextPath + "/moveline-main/detail/setLandmarkImages",
@@ -287,7 +262,6 @@ showContent.addEventListener("click", function () {
 //                 slide2.append(slideImg2);
 //             }
 
-
 //             let pager = document.querySelector(".pager2");
 //             pager.innerHTML = "";
 
@@ -301,12 +275,11 @@ showContent.addEventListener("click", function () {
 //                 }
 //                 pager.append(span);
 
-//                 pager.addEventListener('click',function(event){ 
+//                 pager.addEventListener('click',function(event){
 //                     var pagerNum2 = event.target.innerText - 1;
 //                     goToSlide2(pagerNum2);
 //                 });
 //             }
-
 
 //             $pager2 = document.querySelector('.pager2');
 //             $slide2 = document.querySelectorAll('.slide2');
@@ -318,8 +291,6 @@ showContent.addEventListener("click", function () {
 
 //             // }
 
-
-
 //         },
 
 //         error: function (req, status, error) {
@@ -329,8 +300,6 @@ showContent.addEventListener("click", function () {
 //     });
 // });
 
-
-
 // showContent.addEventListener("click", function () {
 
 //     $.ajax({
@@ -339,7 +308,7 @@ showContent.addEventListener("click", function () {
 //         type: "get",
 //         dataType: "JSON",
 //         success: function (landmarkContent) {
-            
+
 //             console.log("size : " + landmarkImageList[0].landMarkReName);
 //                     const test1 = document.getElementById("test1"); // 사진 아래 제거
 //                     const test2 = document.getElementById("landmark-explain"); // 상세설명 내용 제거
@@ -367,7 +336,6 @@ showContent.addEventListener("click", function () {
 //                         slide2.append(slideImg2);
 //                     }
 
-
 //                     let pager = document.querySelector(".pager2");
 //                     pager.innerHTML = "";
 
@@ -381,12 +349,11 @@ showContent.addEventListener("click", function () {
 //                         }
 //                         pager.append(span);
 
-//                         pager.addEventListener('click',function(event){ 
+//                         pager.addEventListener('click',function(event){
 //                             var pagerNum2 = event.target.innerText - 1;
 //                             goToSlide2(pagerNum2);
 //                         });
 //                     }
-
 
 //                     $pager2 = document.querySelector('.pager2');
 //                     $slide2 = document.querySelectorAll('.slide2');
@@ -398,7 +365,6 @@ showContent.addEventListener("click", function () {
 
 //                     // }
 
-     
 //         }
 //     })
 // });
@@ -408,13 +374,9 @@ showContent.addEventListener("click", function () {
 //     test(oData);
 // });
 
-
-
-
 // 코스 삭제
 // (function(movelineNo){
 //     const deleteBtn = document.getElementById("deleteBtn");
-
 
 //     console.log("deleteBtn clicked");
 
@@ -434,63 +396,55 @@ showContent.addEventListener("click", function () {
 
 // 코스 삭제
 function deleteMoveline(movelineNo) {
+  if (confirm("정말로 삭제 하시겠습니까?")) {
+  } else {
+    return;
+  }
 
-    if (confirm("정말로 삭제 하시겠습니까?")) {
-    } else {
-        return
-    }
+  $.ajax({
+    url: contextPath + "/moveline-main/detail/delete",
+    data: { movelineNo: movelineNo },
+    type: "GET",
+    success: function (result) {
+      console.log("result 값 넘어옴");
 
-    $.ajax({
-        url: contextPath + "/moveline-main/detail/delete",
-        data: { "movelineNo": movelineNo },
-        type: "GET",
-        success: function (result) {
-
-            console.log("result 값 넘어옴");
-
-            if (result > 0) {
-                alert("코스를 삭제하였습니다.");
-                // alert(message);
-                let url = contextPath + "/moveline-main/list"
-                location.href = url;
-            } else {
-                alert("코스를 삭제하지 못하였습니다.");
-                // alert(message);
-            }
-        },
-        error: function (req, status, error) {
-            console.log("코스 삭제 실패");
-            console.log(req.responseText);
-        }
-    });
+      if (result > 0) {
+        alert("코스를 삭제하였습니다.");
+        // alert(message);
+        let url = contextPath + "/moveline-main/list";
+        location.href = url;
+      } else {
+        alert("코스를 삭제하지 못하였습니다.");
+        // alert(message);
+      }
+    },
+    error: function (req, status, error) {
+      console.log("코스 삭제 실패");
+      console.log(req.responseText);
+    },
+  });
 }
-
 
 // 코스 신고
 function reportMoveline(movelineNo) {
+  if (confirm("정말로 신고 하시겠습니까?")) {
+    $.ajax({
+      url: contextPath + "/moveline-main/detail/report",
+      data: { movelineNo: movelineNo },
+      type: "GET",
+      success: function (result) {
+        if (result > 0) {
+          alert("신고 되었습니다");
+          selectReplyList(); // 목록을 다시 조회해서 삭제된 글을 제거
+        } else {
+          alert("신고 실패");
+        }
+      },
 
-    if (confirm("정말로 신고 하시겠습니까?")) {
-
-        $.ajax({
-            url: contextPath + "/moveline-main/detail/report",
-            data: { "movelineNo": movelineNo },
-            type: "GET",
-            success: function (result) {
-                if (result > 0) {
-                    alert("신고 되었습니다");
-                    selectReplyList(); // 목록을 다시 조회해서 삭제된 글을 제거
-                } else {
-                    alert("신고 실패");
-                }
-            },
-
-            error: function (req, status, error) {
-                console.log("실패")
-                console.log(req.responseText);
-            }
-
-        });
-    }
+      error: function (req, status, error) {
+        console.log("실패");
+        console.log(req.responseText);
+      },
+    });
+  }
 }
-
-
